08069e9749cd75df7c61129fff473ce887af6543
Kapil Kanur <kanurkapil@gmail.com>
2023-04-20 12:24:16 +0530

    Fixed null value issue

--- src/main/java/com/kk/device_inventory_management/dtos/DeviceDTO.java
+++ src/main/java/com/kk/device_inventory_management/dtos/DeviceDTO.java
@@ -1,7 +1,11 @@
 package com.kk.device_inventory_management.dtos;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class DeviceDTO {
 
     @NotEmpty
